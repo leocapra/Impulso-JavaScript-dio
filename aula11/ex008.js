e5c473953f7e45a39b518762b2cb1c5ff27c1840
leocapra <leoccapra@gmail.com>
2022-07-21 14:24:21 -0300

    ex concluido

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,6 +1,6 @@
-var vel = 60.5
+var vel = 55
 console.log(`A velocidade do sue carro é ${vel}km/h`)
-if (vel > 60) {
+if (vel > 60) { //condição simples
     console.log('Você ultrapassou a velocidade permitida. MULTADO!')
 }
 console.log('Dirija sempre usando cinto de segurança!')
---
+++ aula11/ex009.js
@@ -0,0 +1,16 @@
+var pais = 'França'
+console.log(`Vivendo em ${pais} `)
+if (pais == 'Brasil') {
+    console.log('Brasileiro')
+} else {
+    console.log('Estrangeiro')
+} 
+
+/*if (pais != 'Brasil) {
+    console.log('Estrangeiro')
+} else {
+    console.log('Brasileiro')
+}
+
+//mesma coisa
+*/
