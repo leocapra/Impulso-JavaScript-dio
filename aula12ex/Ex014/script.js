39194201a7d45cd4d2af39b7eef6433f6fa21d37
leocapra <leoccapra@gmail.com>
2022-07-23 13:39:07 -0300

    exjspt02

--- aula12ex/Ex014/script.js
+++ aula12ex/Ex014/script.js
@@ -1,1 +1,24 @@
-alert('olá')
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var cumprimento = document.getElementById('cumprimento')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA!
+        img.src = 'manha.png'
+        document.body.style.background = '#81b0ec'
+        cumprimento.innerHTML = 'Bom dia!'
+    } else if (hora >= 12 && hora <= 18){
+        //BOA TARDE!
+        img.src = 'tarde.png'
+        document.body.style.background = '#edbf73'
+        cumprimento.innerHTML = 'Boa tarde!'
+    } else {
+        //BOA NOITE!
+        img.src = 'noite.png'
+        document.body.style.background = '#192c53'
+        cumprimento.innerHTML = 'Boa Noite!'
+    }
+}
