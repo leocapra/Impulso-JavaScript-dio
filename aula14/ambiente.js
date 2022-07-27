f9f8f946eeec23cdbaad238ffc78436a1b4c6a65
leocapra <leoccapra@gmail.com>
2022-07-27 17:45:04 -0300

    aula14

---
+++ aula14/ambiente.js
@@ -0,0 +1,22 @@
+/*
+var c = 1
+do {
+    console.log(c)
+    c++
+} while (c <=10)
+*/
+
+//----------------------------- mesma funcionalidade
+/*
+for(var c = 1 ; c<=10; c++) {
+    console.log(c)
+}
+*/
+
+//obs
+
+console.log('vai começar...')
+for(var c = 1 ; c <= 10; c++){
+    console.log(c)
+}
+console.log('fim')
