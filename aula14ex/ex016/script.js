35b39e08aa3249beaed06500d58dc582cd7cc3e9
leocapra <leoccapra@gmail.com>
2022-07-30 16:22:14 -0300

    resolvido

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -5,18 +5,32 @@ var clicar = document.querySelector('button#clicar')
 clicar.addEventListener('click', clicou)
 function clicou() {
     var res = document.querySelector('div#res')
-    const cont = document.querySelector('div#cont')
-    const paragrafo = document.createElement("p")
-    const soma = document.createTextNode(`${inicio}`)
-    if (inicio.value.length == 0 || Number(inicio.value) > Number(fim.value) ) {
+    var cont = document.querySelector('div#cont')
+
+
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = "Impossível contar!"
         alert('[ERRO] Tente colocar alguma informção coerente!')
     } else {
-        if (Number(inicio.value) <= Number(fim.value) && Number(passo.value) >= 0) {
-            while (Number(inicio.value) <= Number(fim.value)) {
-                res.innerHTML = `${Number(inicio.value)} `
-                inicio = inicio + Number(passo.value)
-                cont.innerHTML += paragrafo.innerHTML = `${inicio}`
+        res.innerHTML = "Contando... <br>"
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            //Contagem crescente
+            for (let c = 1; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            //contagem decrescente
+            for(let c = i; c >= f ; c-= p) {
+                res.innerHTML += `${c} \u{1F449}`
             }
         }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
