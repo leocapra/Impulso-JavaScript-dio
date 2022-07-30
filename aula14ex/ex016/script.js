e81b7d02257484d7a58c63feccddd40377919126
leocapra <leoccapra@gmail.com>
2022-07-30 14:33:43 -0300

    tentativa 02

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -4,15 +4,19 @@ var passo = document.querySelector('input#passo')
 var clicar = document.querySelector('button#clicar')
 clicar.addEventListener('click', clicou)
 function clicou() {
-    var ini = Number(inicio)
-    var fi = Number(fim)
-    var pa = Number(pa)
     var res = document.querySelector('div#res')
-    var cont = document.querySelector('div#cont')
-    if (inicio.value.length == 0 || ini >= fi ) {
+    const cont = document.querySelector('div#cont')
+    const paragrafo = document.createElement("p")
+    const soma = document.createTextNode(`${inicio}`)
+    if (inicio.value.length == 0 || Number(inicio.value) > Number(fim.value) ) {
         alert('[ERRO] Tente colocar alguma informção coerente!')
     } else {
-        if (ini <= fi && pa >= 0) {
+        if (Number(inicio.value) <= Number(fim.value) && Number(passo.value) >= 0) {
+            while (Number(inicio.value) <= Number(fim.value)) {
+                res.innerHTML = `${Number(inicio.value)} `
+                inicio = inicio + Number(passo.value)
+                cont.innerHTML += paragrafo.innerHTML = `${inicio}`
+            }
         }
     }
 }
