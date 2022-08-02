93821fcbb5a6b23b462da37283ee7c506474ba6f
leocapra <leoccapra@gmail.com>
2022-08-02 20:50:05 -0300

    vetores

---
+++ aula15/ambiente.js
@@ -0,0 +1,14 @@
+let num = [5,8,2,9,3]
+
+num.sort() //ordena
+num.push(1) //add
+
+console.log(`Nosso vetor é o ${num}`) //mostra a posição atual do vetor na linha
+
+console.log(`O vetor tem ${num.length} posições`) //quantidade de vetor
+
+console.log(`O primeiro valor do ventor é ${num[0]}`) //mostra o primeiro vetorbn
+
+for (let c = 0 ; c < num.length ; c++) {
+    console.log(num[c])
+}  //do alone
