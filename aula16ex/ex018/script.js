def272c20f933b8a56e023914e210a1551ad8670
leocapra <leoccapra@gmail.com>
2022-08-04 18:48:32 -0300

    ex

---
+++ aula16ex/ex018/script.js
@@ -0,0 +1,31 @@
+let add = document.querySelector('button#add')
+let numero = document.querySelector('input#num')
+let most = document.querySelector('select#most')
+let res = document.querySelector('div#res')
+let valores = []
+
+add.addEventListener('click', adicionar)
+
+function thisNumber(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inList(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if (thisNumber(numero.value) && !inList(num.value, valores)) {
+        window.alert('tudo ok')
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
+    }
+}
